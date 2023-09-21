48614ba1022b72600de5dbc7a691919ff313b41c
vinicius-rubia <lucas.de.oliveira.rubia1@gmail.com>
2023-09-20 23:22:50 -0300

    feat: add curriculum

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -46,11 +46,11 @@ export const HeroSection: React.FC = () => {
             >
               Fale comigo
             </Link>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 min-[425px]:mt-0">
+            <a href="/curriculoAtualizado.pdf" download className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3 min-[425px]:mt-0">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Baixar CV
               </span>
-            </button>
+            </a>
           </div>
         </motion.div>
         <motion.div
